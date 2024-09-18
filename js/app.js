61cd335bf0e43e8fd6064db8a0dba3bc6b6c8c7c
Fathiyah Azzahra <fathiyahzzhr@gmail.com>
2024-09-18 14:23:11 +0700

    Komit awal

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    // Example: Toggle navigation menu on mobile
+    $('.menu-toggle').click(function() {
+        $('nav ul').toggleClass('show');
+    });
+    
+    // Example: Load tips or activities
+    $('#daily-tips').html("<p>Remember to breathe deeply and take short breaks today!</p>");
+    });
